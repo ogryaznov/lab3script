ae569e7f7825ad921a73f9faf1c219fd00ee0690
ogryaznov <gryaznovoleg2002@gmail.com>
2022-12-11 17:32:26 +0200

    Integrated mongodb into the project

--- api/start.js
+++ api/start.js
@@ -1,5 +1,12 @@
 const app = require('./server.js');
+const MongoClient = require("mongodb").MongoClient;
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useUnifiedTopology: true});
 
-app.listen(3000, () => {
-	console.log('Server start at localhost:3000');
+mongoClient.connect(function(err, client){
+	if(err) return console.log(err);
+	dbClient = client;
+	app.locals.collection = client.db("usersdb").collection("users");
+	app.listen(3000, function(){
+		console.log("Server expects a connection...");
+	});
 });
